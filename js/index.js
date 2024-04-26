cbfa4598e5d4425664052882807afb4736dc8304
iamyewon <rla357987@gmail.com>
2024-04-26 16:09:36 +0900

    20240426/16:08
    loading, correct, incorrect, startgame, endgame 추가&수정중

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ let wordList = [];
 let limitTime = 20;
 let correctWordList = [];
 let incorrectWordList = [];
+let isLoading = false;
 
 const wordCount = document.querySelector('.word-count');
 const word = document.querySelector('.word');
@@ -16,6 +17,13 @@ const incorrectWords = document.querySelector('.incorrect-words');
 const incorrectCount = document.querySelector('.incorrect-count');
 const resetButton = document.querySelector('.reset-button');
 resetButton.value = setBtnValue();
+const loadingBackground = document.querySelector('.loading-background');
+const loader = document.querySelector('.loader');
+
+// TODO : loading
+loadingBackground.style.display = isLoading ? 'block' : 'none';
+loader.style.display = isLoading ? 'block' : 'none';
+
 
 function fetchData(){
     axios.get(`https://random-word-api.herokuapp.com/word?number=${WORD_COUNT}`)
@@ -23,7 +31,7 @@ function fetchData(){
         wordList = response.data;
         wordCount.textContent = WORD_COUNT;
         word.textContent = wordList[0];
-        // 로딩 
+        isLoading = false;
     }).catch(() => console.error());
 }
 
@@ -45,7 +53,7 @@ function endGame(){
     limitTime = 0;
     // wordTitle.style.visibility = 'hidden';
     wordTitle.textContent = "";
-    word.textContent = "Game Over!";
+    word.textContent = "Game Over";
     time.textContent = `Time's Up!`;
     isEndGame = true;
     resetButton.value = setBtnValue();
@@ -55,6 +63,7 @@ function endGame(){
 
 
 function startGame(){
+    isLoading = true;
     correctWords.textContent = '';
     correctWordList = [];
     incorrectWords.textContent = '';
@@ -63,6 +72,8 @@ function startGame(){
     fetchData(); // 여기도 텀이 생기는데 그냥 원래 받아온 값으로 랜덤돌려줘야할지. 
     wordTitle.textContent = "제시어";
     wordInput.disabled = false;
+    correctCount.textContent = '';
+    incorrectCount.textContent = '';
 }
 
 
@@ -85,8 +96,7 @@ resetButton.addEventListener("click", () => {
 })
 
 
-// TODO : 단어 맞췄을 때 filter 써서 리스트에서 빼주기 
-
+// TODO : filter ,, ?
 function handleCorrectWord(correctWord){
     wordList = wordList.filter((value) => value !== correctWord);
 }
@@ -97,13 +107,14 @@ function handleInputWord(e){
     if(e.key === 'Enter'){
         if(word.textContent === wordInput.value.trim()){
             correctWordList.push(word.textContent);
-            correctWords.textContent += `${word.textContent} `;
+            correctWords.textContent += `${word.textContent}  `;
             correctCount.textContent = `(${correctWordList.length})`;
             handleCorrectWord(word.textContent);
             word.textContent = wordList[0];
+            wordCount.textContent -= 1;
         }else{
             incorrectWordList.push(word.textContent);
-            incorrectWords.textContent += `${wordInput.value.trim()} `;
+            incorrectWords.textContent += `${wordInput.value.trim()}  `;
             incorrectCount.textContent = `(${incorrectWordList.length})`;
         }
         wordInput.value = "";
